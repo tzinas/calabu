b1be12f2dc84c0e7d2cc21de4ff42b9833bfec2f
Apostolos Tzinas <tzinas@tzinas.com>
2021-09-03 14:53:10 +0300

    Check for testing with jest for db

--- db.js
+++ db.js
@@ -1,5 +1,12 @@
 import level from 'level'
 
-const db = level('db-calabu')
+const areWeTestingWithJest = () => {
+  return process.env.JEST_WORKER_ID !== undefined;
+}
+
+let db
+if (!areWeTestingWithJest()) {
+  db = level('db-calabu')
+}
 
 export default db
