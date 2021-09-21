5b02fb10e64d6f814a2c70beb9b997fb7bd17314
Apostolos Tzinas <tzinas@tzinas.com>
2021-09-21 11:14:40 +0300

    Add getBlockHeight and test

--- blockchain.js
+++ blockchain.js
@@ -24,7 +24,27 @@ export class BlockchainManager {
     return this.getFirstAncestorBlock({ blockchain, otherBlock: previousBlock })
   }
 
+  async getBlockHeight({ knownHeightBlock, ancestorBlockHash, knownHeight, requestObject }) {
+    const knownHeightBlockHash = this.blockManager.getBlockHash(knownHeightBlock)
+    if (knownHeightBlockHash === ancestorBlockHash) {
+      return knownHeight
+    }
+
+    let previousBlock
+    try {
+      previousBlock = await requestObject(knownHeightBlock.previd)
+      this.logger('Got this previous block: %O', previousBlock)
+    } catch (e) {
+      this.logger('There was an error getting the previous block: %O', e)
+      return false
+    }
+
+    return await this.getBlockHeight({ knownHeightBlock: previousBlock, ancestorBlockHash, knownHeight: knownHeight - 1, requestObject})
+  }
+
   async handleNewValidBlock({ validBlock, requestObject }) {
+    // check if valid block is already on my chain?
+
     // recursively find first common ancestor block that is already on my longest chain
     const firstAncestorBlock = this.getFirstAncestorBlock({ blockchain: this.blockchain, otherBlock: validBlock })
 
@@ -34,10 +54,15 @@ export class BlockchainManager {
       this.logger('Got this chain tip block: %O', chainTipBlock)
     } catch (e) {
       this.logger('There was an error getting the chain tip block: %O', e)
+      return false
     }
 
     // find the height of the common ancestor block
+    const ancestorBlockHeight = await this.getBlockHeight({ knownHeightBlock: chainTipBlock, ancestorBlockHash: this.blockManager.getBlockHash(firstAncestorBlock), knownHeight: this.chainHeight, requestObject })
+
     // calculate the height of the new block
+    const validBlockHeight = ancestorBlockHeight + -1 * await this.getBlockHeight({ knownHeightBlock: validBlock, ancestorBlockHash: this.blockManager.getBlockHash(firstAncestorBlock), knownHeight: 0, requestObject })
+
     // if (newBlockHeight > chainHeight) make new block the chain tip and change chainHeight
     // remove all blocks from the previous chain tip till the common ancestor block
     // add all blocks from the common ancestor block till the new chain tip
--- tests/blockchain.test.js
+++ tests/blockchain.test.js
@@ -7,6 +7,25 @@ const BLOCK_1 = new Block({ "type": "block", "txids": [ "84685e2f0681e6cd36ca31e
 const BLOCK_2 = new Block({ "type": "block", "txids": [], "nonce": "first", "previd": "ec6d126b393fa7bcb2bd153e8b02f8ecf789aa08793847b67d32dd61c61a5d2b", "created": "1622825645", "T": "00000002af000000000000000000000000000000000000000000000000000000" })
 const BLOCK_3 = new Block({ "type": "block", "txids": [], "nonce": "second", "previd": "00000000a420b7cefa2b7730243316921ed59ffe836e111ca3801f82a4f5360e", "created": "1622825645", "T": "00000002af000000000000000000000000000000000000000000000000000000" })
 
+const blockManager = new BlockManager()
+
+const requestObject = async (objectId) => {
+  if (objectId === blockManager.getBlockHash(GENESIS_BLOCK)) {
+    return GENESIS_BLOCK
+  }
+
+  if (objectId === blockManager.getBlockHash(BLOCK_1)) {
+    return BLOCK_1
+  }
+
+  if (objectId === blockManager.getBlockHash(BLOCK_2)) {
+    return BLOCK_2
+  }
+
+  if (objectId === blockManager.getBlockHash(BLOCK_3)) {
+    return BLOCK_3
+  }
+}
 
 beforeAll(() => {
   logger.transports.forEach(transport => {
@@ -16,25 +35,6 @@ beforeAll(() => {
 
 test('find correct ancestor block', async () => {
   const blockchainManager = new BlockchainManager()
-  const blockManager = new BlockManager()
-
-  const requestObject = async (objectId) => {
-    if (objectId === blockManager.getBlockHash(GENESIS_BLOCK)) {
-      return GENESIS_BLOCK
-    }
-
-    if (objectId === blockManager.getBlockHash(BLOCK_1)) {
-      return BLOCK_1
-    }
-
-    if (objectId === blockManager.getBlockHash(BLOCK_2)) {
-      return BLOCK_2
-    }
-
-    if (objectId === blockManager.getBlockHash(BLOCK_3)) {
-      return BLOCK_3
-    }
-  }
 
   const blockchain = new Set([blockManager.getBlockHash(GENESIS_BLOCK), blockManager.getBlockHash(BLOCK_1), blockManager.getBlockHash(BLOCK_2)])
 
@@ -43,4 +43,12 @@ test('find correct ancestor block', async () => {
   const isValid = blockManager.getBlockHash(GENESIS_BLOCK) === blockManager.getBlockHash(ancestor)
 
   expect(isValid).toBe(true)
+})
+
+test('find correct height of block', async () => {
+  const blockchainManager = new BlockchainManager()
+
+  const blockHeight = await blockchainManager.getBlockHeight({ knownHeightBlock: BLOCK_2, ancestorBlockHash: blockManager.getBlockHash(GENESIS_BLOCK), knownHeight: 3, requestObject })
+
+  expect(blockHeight).toBe(1)
 })
